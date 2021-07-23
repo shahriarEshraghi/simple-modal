cae6b57a05114c041e5e579672b08158fce4dac7
shahriar <shahriareshraghi22@Gmail.com>
2021-07-23 13:07:26 +0430

    project v1

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 ## Features
 
 - you can open or close the modal using Esc or 'X' button or click anywhere out of the modal.
-- behind of the modal you have your screen but it's a bit blur .
+- behind modal you have your screen but it's a bit blur .
--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const buttons = document.querySelectorAll('.show-modal');
+for (let button of buttons) {
+    button.addEventListener('click', function () {
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
+}
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+document.addEventListener('click', e => {
+    if ((e.target.classList.contains('close-modal')) || (!(e.target.classList.contains('show-modal')) && !(e.target.closest('.modal')))) {
+        console.log('BASTE');
+        closeModal();
+    }
+});
