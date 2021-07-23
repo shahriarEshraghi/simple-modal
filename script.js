549cb68cdb89810b590d7fbadd18de00c71a30b5
shahriar <shahriareshraghi22@Gmail.com>
2021-07-23 13:50:18 +0430

    v2

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
+const closeBtn = document.querySelector('.close-modal');
 const buttons = document.querySelectorAll('.show-modal');
 for (let button of buttons) {
     button.addEventListener('click', function () {
@@ -12,9 +13,13 @@ const closeModal = function () {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
 }
-document.addEventListener('click', e => {
-    if ((e.target.classList.contains('close-modal')) || (!(e.target.classList.contains('show-modal')) && !(e.target.closest('.modal')))) {
-        console.log('BASTE');
-        closeModal();
+closeBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape') {
+        if (!modal.classList.contains('hidden')) {
+            closeModal();
+        }
     }
 });
